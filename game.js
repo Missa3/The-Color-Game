24cb2abf8d6c4c49f9a9f2726d82c26202590b78
Missael Romero <missael7994@gmail.com>
2017-03-22 10:22:50 -0700

    added right and wrong

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
+var color = [
+    "rgb(225, 0, 0)",
+    "rgb(0, 225, 0)",
+    "rgb(0, 0, 225)",
+    "rgb(225, 225, 0)",
+    "rgb(225, 0, 225)",
+    "rgb(0, 225, 225)",
+];
 
+var square = document.querySelectorAll('.square');
+var pickedColor = color[0];
+var colorDisplay = document.querySelector('#colorDisplay');
+colorDisplay.innerHTML = pickedColor;
+
+for (var i = 0; i < square.length; i++) {
+    square[i].style.background = color[i];
+
+    square[i].addEventListener('click', function(e) {
+      var colorOfSquare = this.style.background;
+        if (colorOfSquare == pickedColor) {
+            alert('Right! Good Job');
+        } else {
+          alert('Try again');
+        }
+
+    });
+}
